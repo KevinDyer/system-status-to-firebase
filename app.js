9d5b38bdc12d1554f92899e698ca65c244b6318b
Kevin Dyer <kevindy@lgsinnovations.com>
2016-09-07 21:47:14 -0600

    Added sign in and location.

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 (() => {
   'use strict';
 
-  const path        = require('path');
-  const winston     = require('winston');
-  const parseArgs   = require('minimist');
-  const GpsService  = require('./lib/gps-service');
+  process.on('uncaughtException', (e) => console.log(e.stack));
+
+  const path            = require('path');
+  const winston         = require('winston');
+  const parseArgs       = require('minimist');
+  const GpsService      = require('./lib/gps-service');
+  const SignInService   = require('./lib/signin-service');
+  const LocationHistory = require('./lib/location-history');
+  const firebase        = require('firebase');
+
+  const config = {
+    apiKey: "AIzaSyDMRSBYIJRexb7qlH5tCHnG75WV8rG-DBQ",
+    authDomain: "system-status-13c93.firebaseapp.com",
+    databaseURL: "https://system-status-13c93.firebaseio.com",
+    storageBucket: "system-status-13c93.appspot.com",
+  };
+
+  firebase.initializeApp(config);
 
   const args = parseArgs(process.argv, {default: {output: null}, alias: {output: ['o']}});
 
@@ -17,6 +31,7 @@
   }
 
   const logger = new winston.Logger({
+    level: 'debug',
     transports: [
       new winston.transports.Console({timestamps: true, colorize: true, prettyPrint: true}),
       new winston.transports.File({filename: logFilePath})
@@ -25,12 +40,15 @@
 
   class Runner {
     constructor() {
+      this.signInService = new SignInService();
       this.gpsService = new GpsService();
+      this.locationHistory = new LocationHistory(this.signInService, this.gpsService);
     }
 
     start() {
       logger.debug('start');
-      return this.gpsService.start({reconnect: true});
+      return this.signInService.signIn()
+      .then(() => this.gpsService.start({reconnect: true}));
     }
 
     stop() {
